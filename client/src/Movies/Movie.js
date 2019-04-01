909bfedbd9601fd75257e95f6beabd949c55895a
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-01 16:29:00 -0400

    Fixes path typo on App.js, completes MovieList.js & Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,13 +5,14 @@ export default class Movie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: null
+      movie: null,
+      id: `${props.match.params.id}`,
     };
   }
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.state.id;
     this.fetchMovie(id);
   }
 
